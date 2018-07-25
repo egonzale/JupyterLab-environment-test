e14412597d6683739c9e8e7d499b1e94acb314df
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2018-07-25 17:18:54 +0300

    Add tests

--- installations.bash
+++ installations.bash
@@ -5,5 +5,7 @@ export GIT_COMMITTER_NAME=htenkanen
 export GIT_COMMITTER_EMAIL=henrikki.tenkanen@helsinki.fi
 git clone https://github.com/geo-python/JupyterLab-environment-test.git
 
+pip install geopandas
+
 # Install geopandas using conda
-conda install -c conda-forge geopandas=0.4.0
+conda install -c conda-forge shapely
