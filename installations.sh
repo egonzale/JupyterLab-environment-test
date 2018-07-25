ee30327afdd6892daeab05783b383907a7884114
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2018-07-25 17:35:39 +0300

    Add test stuff

--- installations.sh
+++ installations.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd /home/jovyan/work
+# git reflog requires a name and email if user is not in passwd
+# even if you're only cloning
+export GIT_COMMITTER_NAME=anonymous
+export GIT_COMMITTER_EMAIL=anon@localhost
+
 # Configure
 git clone https://github.com/geo-python/JupyterLab-environment-test.git
 
